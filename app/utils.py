7a38cffedb622a1600c59e6c0254faa96ab6dd87
carmitbe1@gmail.com <carmit@terasky.com>
2021-10-08 14:00:13 +0300

    add dynamodb region

--- app/utils.py
+++ app/utils.py
@@ -6,10 +6,10 @@ import os
 #dynamodb_host = "localhost" #"54.216.66.226"
 #dynamodb = boto3.resource('dynamodb',endpoint_url='http://' + dynamodb_host + ':8000')
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url='http://localhost:8000')
-session = boto3.session('dynamodb',
-                        aws_access_key_id="DUMMY_ACCESS_ID",
+session = boto3.session(aws_access_key_id="DUMMY_ACCESS_ID",
                         aws_secret_access_key="DUMMY_SECRET_KEY",
                         region_name="us-west-2")
+
 def get_secret():
   table = dynamodb.Table('devops-challenge')      
   resp = table.get_item(
